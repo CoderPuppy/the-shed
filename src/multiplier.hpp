674a5419fa96455249a8dad279ef958d6545ce03
CoderPuppy <coderpup@gmail.com>
2022-04-18 10:50:25 -0400

    I forgot these files

---
+++ src/multiplier.hpp
@@ -0,0 +1,21 @@
+// multiplier.hpp
+// Author: Drew Young (ajy2158@rit.edu)
+//
+// Implementation of a multiplier. 16-bit times 16-bit to 32-bit output on two
+// 16-bit registers.
+
+#ifndef multiplier_hpp
+#define multiplier_hpp
+
+#include "includes.hpp"
+
+void mult_connect();
+void mult_setup(StorageObject& op);
+void mult_tick0(StorageObject& op);
+void mult_tick1();
+void mult_tick2();
+void mult_tick3();
+void mult_tick4();
+void mult_tick5();
+
+#endif
